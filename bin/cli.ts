7d5bf2780209b638dea782c6702136e1d1c29abc
g. nicholas d'andrea <nick@gnidan.org>
2023-04-28 21:44:55 -0400

    Start preparing to rebuild as a library

--- bin/cli.ts
+++ bin/cli.ts
@@ -3,5 +3,4 @@ import React from "react";
 import {render} from "ink";
 import App from "../src/App.js";
 
-
 render(React.createElement(App));
--- src/cli/Scroller.tsx
+++ src/cli/Scroller.tsx
@@ -33,26 +33,42 @@ export const Scroller: React.FC<Props> = ({
     width: 0
   });
 
-  const [top, setTop] = useState(0);
-  const [left, setLeft] = useState(0);
+  const [position, setPosition] = useState<{ top: number, left: number }>({
+    top: 0,
+    left: 0
+  });
 
   const handleCommand = (options: { command: Command }) => {
+    const { top, left } = position;
+
     const { command } = options;
     switch (command.type) {
       case "up": {
-        setTop(Math.max(0, top - 1));
+        setPosition({
+          top: Math.max(top - 1, 0),
+          left
+        });
         break;
       }
       case "down": {
-        setTop(Math.min(layout.height, top + 1));
+        setPosition({
+          top: Math.min(top + 1, layout.height),
+          left
+        });
         break;
       }
       case "left": {
-        setLeft(Math.max(0, left - 1));
+        setPosition({
+          top,
+          left: Math.max(left -1, 0)
+        });
         break;
       }
       case "right": {
-        setLeft(Math.min(layout.width, left + 1));
+        setPosition({
+          top,
+          left: Math.min(left + 1, layout.width)
+        });
         break;
       }
     }
@@ -76,8 +92,8 @@ export const Scroller: React.FC<Props> = ({
           ref={ref}
           flexShrink={0}
           flexDirection="column"
-          marginTop={-top}
-          marginLeft={-left}
+          marginTop={-position.top}
+          marginLeft={-position.left}
           width={layout.width - 2}
         >
           {children}
@@ -86,31 +102,8 @@ export const Scroller: React.FC<Props> = ({
       <Footer
         onLayout={setFooterDimensions}
         onCommand={handleCommand}
-        bodyPosition={{top, left}}
+        bodyPosition={position}
         />
     </Box>
   );
-
-  {/* return <Box */}
-  {/*   ref={ref} */}
-  {/*   height={height} */}
-  {/*   width={width} */}
-  {/*   flexDirection="column" */}
-  {/*   borderStyle="round" */}
-  {/* > */}
-  {/*   <Box */}
-  {/*     height={layout.height - footerDimensions.height - 2} */}
-  {/*     width={layout.width - 2 /* (for border) *1/} */}
-  {/*     overflow="hidden" */}
-  {/*   > */}
-  {/*     <Box marginTop={-top} marginLeft={-left}> */}
-  {/*       {children} */}
-  {/*     </Box> */}
-  {/*   </Box> */}
-  {/*   <Footer */}
-  {/*     onLayout={setFooterDimensions} */}
-  {/*     onCommand={handleCommand} */}
-  {/*     bodyPosition={{top, left}} */}
-  {/*     /> */}
-  {/* </Box>; */}
 };
